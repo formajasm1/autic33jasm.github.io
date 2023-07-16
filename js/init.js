1d32407a49bae66ab0b575ec5e1dc77bbd765d27
formajasm1 <formajasm1@gmail.com>
2023-07-15 19:16:29 -0600

    Cambios

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyA165uiIOJxI_SJUZ1ey6S8D2r52_2rmAM",
+  authDomain: "autentic33jasm.firebaseapp.com",
+  projectId: "autentic33jasm",
+  storageBucket: "autentic33jasm.appspot.com",
+  messagingSenderId: "1024828845579",
+  appId: "1:1024828845579:web:ec1285cba2ded0123a610e",
+  measurementId: "G-92V9V17JEP"
+    });
